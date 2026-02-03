0277d5fd39f2a4b9d4f263838f4d2114a091722e
jonata <jonata.a@outlook.com>
2026-02-02 21:01:32 -0300

    chore: spring setup

---
+++ src/test/java/com/jonataalbuquerque/spring_ai_chat/SpringAiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonataalbuquerque.spring_ai_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
